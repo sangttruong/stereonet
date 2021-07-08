12be953fc96cf88cbc23c98a020c6eb0cddbe105
Sang T. Truong <sttruong@stanford.edu>
2021-07-07 21:51:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='steronet,
+    name='stereonet',
     packages=find_packages(),
     version='0.1.0',
     description='An Attention Graph Neural Network for Stereo-active Molecules',
     author='Sang TTruong',
-    author_email="sttruong@stanford.edu"
+    author_email="sttruong@stanford.edu",
     license='MIT',
     
     url="",
@@ -19,8 +19,5 @@ setup(
         "Operating System :: OS Independent",
 
     ],
-
-    python_requires='>=3.5',
-    package_data=
 )
 
